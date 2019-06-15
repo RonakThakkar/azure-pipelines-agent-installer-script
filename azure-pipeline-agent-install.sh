e6e9bbb43b2d14315daa89d3811648961b184881
Thakkar Ronak <ronak.thakkar@tieto.com>
2019-06-15 17:42:25 +0530

    changed

--- azure-pipeline-agent-install.sh
+++ azure-pipeline-agent-install.sh
@@ -43,8 +43,7 @@ if [ -z "$4" ]
     agentName=$4
 fi
 
-cd /home/azuredevopsuser
-cd agent
+cd /home/azuredevopsuser/agent
 
 sudo -u azuredevopsuser ./config.sh --unattended --url $azureDevOpsServerUrl --auth pat --token $pat --pool $agentPool --agent $agentName --acceptTeeEula --work ./_work --runAsService
 echo "configuration done"
